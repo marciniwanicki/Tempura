0c0935dc08d9a440d935c4f468eab0774aadaf0b
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2018-01-27 12:37:39 +0000

    Test Directories

---
+++ Tests/Nodes/DictionariesTests.swift
@@ -0,0 +1,43 @@
+//
+// Created by Marcin Iwanicki on 27/01/2018.
+//
+
+import Foundation
+
+import XCTest
+@testable import Tempura
+
+class DictionariesTests: XCTestCase {
+
+  func testListWhenRootOnly() {
+    // given
+    let sut = Directories(rootInodeId: 1)
+    let expected = [".": 1, "..": 1]
+
+    // when / then
+    XCTAssertEqual(expected, sut.list(inodeId: 1)!)
+  }
+
+  func testAddRootSubdirectory() {
+    // given
+    let sut = Directories(rootInodeId: 1)
+
+    // when
+    let result = sut.add(inode: (2, "filename"), parentInodeId: 1)
+
+    // then
+    XCTAssertEqual(Result.success(value: 2), result)
+    XCTAssertEqual([".": 1, "..": 1, "filename": 2], sut.list(inodeId: 1)!)
+  }
+
+  func testAddWhenParentDoesNotExist() {
+    // given
+    let sut = Directories(rootInodeId: 1)
+
+    // when
+    let result = sut.add(inode: (2, "filename"), parentInodeId: 3)
+
+    // then
+    XCTAssertEqual(Result.failure(reason: .inodeNotFound), result)
+  }
+}
